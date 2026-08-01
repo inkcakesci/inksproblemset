128aaa001dd2945da73ad414643578abde8af643
inkcakesci <guirongrestaurant@gmail.com>
2026-08-01 14:54:13 +0800

    Fix GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.GITHUB_ACTIONS ? '/InksProblemSet/' : '/',
+  base: process.env.GITHUB_ACTIONS ? '/inksproblemset/' : '/',
 })
